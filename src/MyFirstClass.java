952c0c58f0d104477fcd783a010afc3a88a5399b
BerserkZak <me4anicman@gmail.com>
2023-02-25 17:30:14 +0300

    Painter

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -5,6 +5,30 @@ public class MyFirstClass {
     }
 
 
+
+    //Painter
+/*    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+        double remainingBuckets = (width * height) - (areaPerBucket * extraBuckets);
+        return (int) ((int) Math.ceil(remainingBuckets / areaPerBucket));
+    }
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double bucketTotal = Math.ceil(width * height / areaPerBucket);
+        return (int) bucketTotal;
+    }
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double areaTotal = Math.ceil(area / areaPerBucket);
+        return (int) areaTotal;
+    }*/
+
     //InputCalculator
 /*
     public static void inputThenPrintSumAndAverage() {
