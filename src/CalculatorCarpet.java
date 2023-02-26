b13c8c793754a660fb73c8789fb803002cd54950
BerserkZak <me4anicman@gmail.com>
2023-02-26 21:55:48 +0300

    CarpetCalculation Exercise

---
+++ src/CalculatorCarpet.java
@@ -0,0 +1,13 @@
+public class CalculatorCarpet {
+    private Floor floor;
+    private Carpet carpet;
+
+    public CalculatorCarpet(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea()*carpet.getCost();
+    }
+}
